bc6fd4da1dda35370a83058a231e8b51b12198fd
stephting8 <sjt88steph@gmail.com>
2024-12-11 14:33:45 -0600

    Initial Commit

---
+++ LokeRecipes/LokeRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LokeRecipesApp.swift
+//  LokeRecipes
+//
+//  Created by Stephanie Ting on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LokeRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LokeRecipesTests/LokeRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  LokeRecipesTests.swift
+//  LokeRecipesTests
+//
+//  Created by Stephanie Ting on 12/11/24.
+//
+
+import Testing
+@testable import LokeRecipes
+
+struct LokeRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
